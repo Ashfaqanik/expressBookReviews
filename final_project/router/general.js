d3cc19017942a4912f3296b3348b30192ed1a475
Ashfaque <ashfaqanik698@gmail.com>
2024-06-28 19:05:28 +1000

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,7 +39,13 @@ public_users.get("/author/:author", function (req, res) {
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+
+  const keys = Object.keys(books);
+  const booksByTitle = keys
+    .map((key) => books[key])
+    .filter((book) => book.title === title);
+  res.send(booksByTitle);
 });
 
 //  Get book review
